bdc32f17072e6581866dd182be249aaf8c6f9dac
YoonKeumJae <ykj1512@gmail.com>
2024-06-14 10:26:01 +0900

    [Feat] Basic layout setting

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ function App() {
   return (
     <>
       <h1>The TODOLIST Project</h1>
+      <form>
+        <input type="text" placeholder="Input your todos" />
+        <button>OK</button>
+      </form>
+      <div>
+        <ul>
+          <li>Todo 1 <button>X</button></li>
+          <li>Todo 2 <button>X</button></li>
+          <li>Todo 3 <button>X</button></li>
+        </ul>
+      </div>
     </>
   )
 }
