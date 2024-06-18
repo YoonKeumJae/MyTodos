7b199a6066c47676d3d7d41d5bdb09d565fd698f
YoonKeumJae <ykj1512@gmail.com>
2024-06-18 14:07:07 +0900

    [Feat] Global style add

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { GlobalStyles } from "./GlobalStyle.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+);
